a4258dffafc38a4f5226801e6b961c83a3604e85
stevep <stevep_pan@yahoo.com>
2025-05-14 18:08:06 +0300

    feat: add hello world

---
+++ src/gr/aueb/cf/helloapp/HelloWorld.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.helloapp;
+
+/**
+ * This project created to show how
+ * to make a git project. And the how
+ * to stage and commit changes in local repo.
+ * And the how to push to GitHub.
+ */
+public class HelloWorld {
+}
